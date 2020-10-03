7d822913af4a4c64604e6028ef6b8a1aa50140aa
ykerus <48921025+ykerus@users.noreply.github.com>
2020-10-03 13:51:33 +0200

    Update DQN_plots.py

--- scripts/DQN_plots.py
+++ scripts/DQN_plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Code structure based on lab4 from Reinforcement Learning course at University of Amsterdam
 
 
-def plot_smooth(x, N):
+def plot_smooth(x, N, show=False):
     cumsum = np.cumsum(np.insert(x, 0, 0)) 
     plt.plot((cumsum[N:] - cumsum[:-N]) / float(N))
 
