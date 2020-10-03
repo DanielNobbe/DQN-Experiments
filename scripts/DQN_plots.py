7cb82a28d5a5b3aa4ec36b3af1cea0cad3e3014c
lennertjansen <lennertjansen95@gmail.com>
2020-10-03 12:00:35 +0200

    Added option to show plots to plot_smooth()

--- scripts/DQN_plots.py
+++ scripts/DQN_plots.py
@@ -7,3 +7,6 @@ import matplotlib.pyplot as plt
 def plot_smooth(x, N):
     cumsum = np.cumsum(np.insert(x, 0, 0)) 
     plt.plot((cumsum[N:] - cumsum[:-N]) / float(N))
+
+    if show:
+        plt.show()
