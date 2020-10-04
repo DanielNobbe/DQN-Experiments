6c519b20535e55c1c4fc046c5d0f498defc497da
ykerus <48921025+ykerus@users.noreply.github.com>
2020-10-04 15:22:53 +0200

    Ensure correct in/output size DQN model

--- scripts/DQN_model.py
+++ scripts/DQN_model.py
@@ -10,16 +10,18 @@ class QNetwork(nn.Module):
         nn.Module.__init__(self)
         self.l1 = nn.Linear(in_size, num_hidden)
         self.l2 = nn.Linear(num_hidden, out_size)
+        self.in_size = in_size
+        self.out_size = out_size
 
     def forward(self, x):
-        if len(x.shape) == 0:
-            x = x.unsqueeze(dim=0)
-        x = torch.tensor(x) # Seems like this does nothing, even when numpy gets passed into Q
+
+        x = torch.tensor(x)  # Seems like this does nothing, even when numpy gets passed into Q
+        x = x.view(-1, self.in_size)
+
         layer_1 = self.l1(x)
         hidden = nn.functional.relu(layer_1) # Apply activation function as function rather than later
-        output = self.l2(hidden)
-        if len(output.shape) == 1:
-            output = output.unsqueeze(dim=0)
+        output = self.l2(hidden).view(-1, self.out_size)
+
         return output
 
 
